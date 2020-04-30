c55759a8a33385832685d2a02c4b666ef317fc82
Jeon, Yeo Hun <21500630@handong.edu>
2020-04-30 20:48:54 +0900

    additem implemet done

---
+++ assignment/finalexam/lib/additem.dart
@@ -0,0 +1,153 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:uuid/uuid.dart';
+
+class AddItemPage extends StatelessWidget {
+  TextEditingController titleText = TextEditingController();
+  TextEditingController priceText = TextEditingController();
+  TextEditingController desText = TextEditingController();
+  String uid;
+  final String uuid = Uuid().v1();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            FlatButton(
+              child: Text(
+                'Cancel',
+                style: TextStyle(fontSize: 15),
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            Text('Add'),
+            FlatButton(
+              child: Text('Save'),
+              onPressed: () {
+                createItem();
+                uploadImage();
+                titleText.clear();
+                priceText.clear();
+                desText.clear();
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          SetImage(),
+          Container(
+            padding: EdgeInsets.all(50),
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(labelText: 'Product Name'),
+                  controller: titleText,
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: 'Price'),
+                  controller: priceText,
+                ),
+                TextField(
+                  decoration: InputDecoration(labelText: 'Description'),
+                  controller: desText,
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void createItem() async {
+    final FirebaseUser userID = await FirebaseAuth.instance.currentUser();
+    uid = userID.uid;
+
+    Firestore.instance.collection('items').add({
+      'name': titleText.text,
+      'description': desText.text,
+      'price': priceText.text,
+      'votes': 0,
+      'uid': uid,
+      'createdTime': FieldValue.serverTimestamp(),
+      'ModTime' : FieldValue.serverTimestamp(),
+      'imageID' : uuid,
+    });
+  }
+
+  Future<void> uploadImage() async {
+    File img = _SetImageState._image;
+    StorageReference ref =
+        FirebaseStorage.instance.ref().child(uuid).child('image.jpg');
+    ref.putFile(img);
+  }
+
+
+}
+
+class SetImage extends StatefulWidget {
+  _SetImageState createState() => _SetImageState();
+}
+
+class _SetImageState extends State<SetImage> {
+  static File _image;
+  getGalleryImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      _image = image;
+    });
+  }
+
+  getCameraImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+    setState(() {
+      _image = image;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          height: 250,
+          child: _image == null
+            ? Image.network(
+                'http://handong.edu/site/handong/res/img/logo.png',
+                fit: BoxFit.scaleDown,
+              )
+            : Image.file(_image),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.camera_alt),
+              onPressed: () {
+                getGalleryImage();
+              },
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+
